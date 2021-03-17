db98c5efa2ac41f5d874d4332e2fbc5ec562b05b
Mammadsafar <72881511+Mammadsafar@users.noreply.github.com>
2021-03-17 13:10:28 +0330

    change windows and start codeing again

--- login whit session and cookies/app.js
+++ login whit session and cookies/app.js
@@ -25,7 +25,9 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -39,14 +41,15 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-      expires: 600000
+    expires: 600000
   }
 }));
 
 app.use((req, res, next) => {
-  if (req.cookies.user_sid && !req.session.user){
+  if (req.cookies.user_sid && !req.session.user) {
     res.clearCookie('user_sid');
-  }next();
+  }
+  next();
 })
 
 
@@ -54,12 +57,12 @@ app.use((req, res, next) => {
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
